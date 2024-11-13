7f35c0fffac83a745d5df1e43ece5cd64a9d83bc
Mariana <marychris.crf@gmail.com>
2024-11-12 22:11:11 -0300

    estilizando o header e footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ import { SideMenu } from '../Menu/Menu';
 
 export const Header = () => {
     return (
-        <header className="flex items-center justify-between p-4 bg-gray-800 text-white h-16">
+        <header className="flex items-center justify-between p-4 bg-white text-white h-16">
             <Logo />
             <SideMenu />
         </header>
