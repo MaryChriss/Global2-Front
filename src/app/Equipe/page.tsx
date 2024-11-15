387b993dd8a0206f33d9f6579276b706ae5c2919
Henrique <mrmmjs70@gmail.com>
2024-11-15 13:19:58 -0300

    ajuste no menu e dando inicio as paginas de login

---
+++ src/app/Equipe/page.tsx
@@ -0,0 +1,5 @@
+export default function Equipe() {
+    return(
+        <h1>oi equipe</h1>
+    )
+}
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,10 +1,9 @@
-
 "use client";
 
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { BsGraphUpArrow } from 'react-icons/bs';
-import { FaHome, FaUser } from 'react-icons/fa';
+import { FaHome, FaUser, FaUserEdit } from 'react-icons/fa';
 import { IoMenu } from 'react-icons/io5';
 import { RiTeamFill } from 'react-icons/ri';
 
@@ -21,39 +20,43 @@ export const SideMenu = () => {
                 <IoMenu />
             </button>
 
-                <div
-                    className={`fixed top-0 right-0 h-full bg-white text-white border-slate-300 border transition-all duration-300 ease-in-out
-                                ${isOpen ? 'w-64' : 'w-0 overflow-hidden'}`}
-                >
-
-                <div className="p-4">
+            <div
+                className={`fixed top-0 right-0 h-full bg-white text-white border-slate-300 border transition-all duration-300 ease-in-out
+                ${isOpen ? 'w-64' : 'w-0 overflow-hidden'}`}
+            >
+                <div className="p-4 flex flex-col h-full">
                     <h2 className="text-2xl text-black font-semibold mb-6 font-raleway">Menu</h2>
-                    <ul className="space-y-4">
-
+                    <ul className="space-y-4 flex-grow">
                         <li>
                             <Link href="/" className="block px-2 py-1 rounded font-itim text-black hover:bg-lime-100">
-                            <FaHome size={21} className="inline-block mr-2 text-lime-600" /> Página Inicial
+                                <FaHome size={21} className="inline-block mr-2 text-lime-600" /> Página Inicial
                             </Link>
                         </li>
 
                         <li>
                             <Link href="/dashbord" className="block px-2 py-1 rounded font-itim text-black hover:bg-lime-100">
-                            <BsGraphUpArrow size={21} className="inline-block mr-2 text-lime-600" /> Dashboard
+                                <BsGraphUpArrow size={21} className="inline-block mr-2 text-lime-600" /> Dashboard
                             </Link>
                         </li>
 
                         <li>
                             <Link href="/perfil" className="block px-2 py-1 rounded font-itim text-black hover:bg-lime-100">
-                            <FaUser size={21} className="inline-block mr-2 text-lime-600" />    Perfil
+                                <FaUser size={21} className="inline-block mr-2 text-lime-600" /> Perfil
                             </Link>
                         </li>
 
                         <li>
                             <Link href="/equipe" className="block px-2 py-1 rounded font-itim text-black hover:bg-lime-100">
-                            <RiTeamFill size={24} className="inline-block mr-2 text-lime-600" />    Equipe
+                                <RiTeamFill size={24} className="inline-block mr-2 text-lime-600" /> Equipe
                             </Link>
                         </li>
                     </ul>
+
+                    <div className="mt-auto">
+                        <Link href="/login" className="block px-2 py-1 rounded font-itim text-black hover:bg-lime-100">
+                            <FaUserEdit size={22} className="inline-block mr-2 text-lime-600" /> Login
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
